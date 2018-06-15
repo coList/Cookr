4ff6cf58bafede96fc34607bcc05b41e2e5eb932
Corinna List <corinna.list@hs-augsburg.de>
2018-06-15 10:25:41 +0200

    Kategorien, Herkunftsland, A bis Z
    Menu + Seiten Upate

--- models/menu.py
+++ models/menu.py
@@ -7,7 +7,21 @@
 
 response.menu = [
     (T('Home'), False, URL('default', 'index'), []),
-    (T('Alle Rezepte'), False, URL('default', 'recipeList', args=(0)), []),
     (T('Eigene Rezepte verwalten'), False, URL('default', 'manageRecipes'), []),
     (T('Neues Rezept'), False, URL('default', 'newRecipe'), []),
+    (T('Rezepte von A bis Z'), False, URL('default', 'recipeList', args = (0)), [])
 ]
+
+#Menu-Item: "Rezepte nach Kategorien": Add all items from db.category
+categories = []
+for row in db(db.category).select():
+    categories.append((T(row.name), False, URL('default', 'categoryRecipeList', args = (row.id, 0)), []))
+
+response.menu.append((T('Rezepte nach Kategorien'), False, None, categories))
+
+#Menu-Item: "Rezepte nach Herkunftsland": Add all items from db.origin
+origins = []
+for row in db(db.origin).select():
+    origins.append((T(row.name), False, URL('default', 'originRecipeList', args = (row.id, 0)), []))
+    
+response.menu.append((T('Rezepte nach Herkunftsland'), False, None, origins))
